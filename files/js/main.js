c9698674009eb86c080fa82c3e56ad4c5c2e7146
luke <99141867+lukxee2@users.noreply.github.com>
2023-04-09 00:50:13 +0100

    Perspective 3D

--- files/js/main.js
+++ files/js/main.js
@@ -19,3 +19,40 @@ let e = 0,
         };
       s();
 
+      // var root = document.querySelector(':root');
+      // root.style.setProperty('--rotation', 0); 
+      // var width = document.body.clientWidth;
+      // document.addEventListener("mousemove", function(event) {
+      //   let calc = ((event.clientX/width) * 15 - 7.5)
+      //   root.style.setProperty('--rotation', calc); 
+      // });
+
+      let e2 = 0,
+          s2 = 0;
+        document.addEventListener(
+          "mousemove",
+          (s2) => {
+            e2 = -15 * (s2.clientX / window.innerWidth - 0.5);
+          },
+          !1
+        );
+        let t2 = () => {
+          requestAnimationFrame(t2),
+            (s2 += 0.1 * (e2 - s2)),
+            (document.querySelector(
+              ".section-panel"
+            ).style.transform = `perspective(5000px) rotateY(${s2}deg)`),
+            (document.querySelector(
+              ".section-starring"
+            ).style.transform = `perspective(5000px) rotateY(${s2}deg)`),
+            (document.querySelector(
+              ".section-edvin"
+            ).style.transform = `perspective(5000px) rotateY(${s2}deg)`),
+            (document.querySelector(
+              ".section-ryding"
+            ).style.transform = `perspective(5000px) rotateY(${s2}deg)`),
+            (document.querySelector(
+              ".section-artist"
+            ).style.transform = `perspective(5000px) rotateY(${s2}deg)`);
+        };
+        t2();
